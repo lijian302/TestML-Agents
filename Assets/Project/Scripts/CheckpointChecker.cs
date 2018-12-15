8bc8107b2dae03b70ad264a78092faa24c5d58c6
袁剑伟 <251410454@qq.com>
2018-12-15 14:06:42 +0800

    测试机器学习

---
+++ Assets/Project/Scripts/CheckpointChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointChecker : MonoBehaviour
+{
+    [HideInInspector]
+    public bool enteredCheckpoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            enteredCheckpoint = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            enteredCheckpoint = false;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/PlatformerAgent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using Gamekit2D;
+
+public class PlatformerAgent : Agent
+{
+    public GameObject m_Agent;
+    public GameObject m_Goal;
+    private PlayerCharacter playerCharacter;
+    private Vector2 initialPosition;
+
+    public override void InitializeAgent()
+    {
+        playerCharacter = m_Agent.GetComponent<PlayerCharacter>();
+        initialPosition = m_Agent.transform.position;
+    }
+
+    public override void CollectObservations()
+    {
+        base.CollectObservations();
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        base.AgentAction(vectorAction, textAction);
+    }
+
+    public override void AgentReset()
+    {
+        base.AgentReset();
+    }
+
+    public override void AgentOnDone()
+    {
+        base.AgentOnDone();
+    }
+
+    public bool IsGrounded(Vector2 position)
+    {
+        Vector2 adjustedPosition = new Vector2(position.x, position.y + 0.5f);
+        Vector3 direction = -Vector3.up;
+        LayerMask layerMask = 1 << 31;
+        RaycastHit2D raycast = Physics2D.Raycast(position, direction, 1, layerMask);
+        return raycast;
+    }
+
+    public bool IsHitWall(Vector2 position)
+    {
+        Vector2 centerPosition = new Vector2(position.x, position.y + 0.5f);
+        Vector3 direction = Vector3.forward;
+        LayerMask layerMask = 1 << 31;
+        RaycastHit2D raycast = Physics2D.Raycast(position, direction, 1, layerMask);
+
+        if (raycast && playerCharacter.CheckForGrounded())
+        {
+            return true;
+        }
+        return false;
+    }
+}
